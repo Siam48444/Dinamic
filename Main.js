f3f336a8cb3c2a04041d560c04fe17c1179986c5
Siam <siam48444@gmail.com>
2024-06-17 12:51:11 +0600

    Main

--- Main.js
+++ Main.js
@@ -22,10 +22,10 @@ const hamburger_btn = document.querySelector(".hamburger_btn");
 hamburger_btn.addEventListener("click", () => {
     const hamburger_att = hamburger_btn.getAttribute("data-clicked");
 
-    if ((hamburger_att = "false")) {
+    if (hamburger_att == "false") {
         hamburger_btn.setAttribute("data-clicked", "true");
     }
-    if ((hamburger_att = "true")) {
+    if (hamburger_att == "true") {
         hamburger_btn.setAttribute("data-clicked", "false");
     }
 });
