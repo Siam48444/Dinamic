0ee061e5d8546fb5bac489228b0382ec85539e90
Siam <siam48444@gmail.com>
2024-06-19 12:35:10 +0600

    Main

--- Main.js
+++ Main.js
@@ -16,37 +16,44 @@ var tl = gsap.timeline();
 const hamburger_btn = document.querySelector(".hamburger_btn");
 
 hamburger_btn.addEventListener("click", () => {
-    const menu = document.querySelector(".menu");
     const hamburger_line = document.querySelectorAll(".hamburger_line");
-    const hamburger_att = hamburger_btn.getAttribute("data-clicked");
 
-    if (hamburger_att == "false") {
-        hamburger_btn.setAttribute("data-clicked", "true");
-
-        // Hamburger openned animations.
-        tl.to(hamburger_line[0], { y: "0.225em", duration: 0.3, ease: "back.out" });
-        tl.to(hamburger_line[1], { y: "-0.225em", duration: 0.3, ease: "back.out" }, "<");
-
-        tl.to(hamburger_line[0], { rotate: "-45deg", duration: 0.3, ease: "back.out" });
-        tl.to(hamburger_line[1], { rotate: "45deg", duration: 0.3, ease: "back.out" }, "<");
-
-        // Menu opening.
-        gsap.to(menu, { y: 0, duration: 0.8, ease: "power3.out" });
-    }
-    if (hamburger_att == "true") {
-        hamburger_btn.setAttribute("data-clicked", "false");
-
-        // Hamburger closed animations.
-        tl.to(hamburger_line[0], { rotate: "0", duration: 0.3, ease: "back.out" });
-        tl.to(hamburger_line[1], { rotate: "0", duration: 0.3, ease: "back.out" }, "<");
-
-        tl.to(hamburger_line[0], { y: "-0.015em", duration: 0.3, ease: "back.out" });
-        tl.to(hamburger_line[1], { y: "0.015em", duration: 0.3, ease: "back.out" }, "<");
-
-        // Menu closing.
-        gsap.to(menu, { y: "-100%", duration: 0.8, ease: "power3.in" });
-    }
+    hamburger_line[0].classList.toggle("line_1_clicked");
+    hamburger_line[1].classList.toggle("line_2_clicked");
 });
+
+// hamburger_btn.addEventListener("click", () => {
+//     const menu = document.querySelector(".menu");
+//     const hamburger_line = document.querySelectorAll(".hamburger_line");
+//     const hamburger_att = hamburger_btn.getAttribute("data-clicked");
+
+//     if (hamburger_att == "false") {
+//         hamburger_btn.setAttribute("data-clicked", "true");
+
+//         // Hamburger openned animations.
+//         tl.to(hamburger_line[0], { y: "0.225em", duration: 0.3, ease: "back.out" });
+//         tl.to(hamburger_line[1], { y: "-0.225em", duration: 0.3, ease: "back.out" }, "<");
+
+//         tl.to(hamburger_line[0], { rotate: "-45deg", duration: 0.3, ease: "back.out" });
+//         tl.to(hamburger_line[1], { rotate: "45deg", duration: 0.3, ease: "back.out" }, "<");
+
+//         // Menu opening.
+//         gsap.to(menu, { y: 0, duration: 0.8, ease: "power3.out" });
+//     }
+//     if (hamburger_att == "true") {
+//         hamburger_btn.setAttribute("data-clicked", "false");
+
+//         // Hamburger closed animations.
+//         tl.to(hamburger_line[0], { rotate: "0", duration: 0.3, ease: "back.out" });
+//         tl.to(hamburger_line[1], { rotate: "0", duration: 0.3, ease: "back.out" }, "<");
+
+//         tl.to(hamburger_line[0], { y: "-0.015em", duration: 0.3, ease: "back.out" });
+//         tl.to(hamburger_line[1], { y: "0.015em", duration: 0.3, ease: "back.out" }, "<");
+
+//         // Menu closing.
+//         gsap.to(menu, { y: "-100%", duration: 0.8, ease: "power3.in" });
+//     }
+// });
 
 // Appearing on scroll animation.
 const scroll_appear = document.querySelectorAll(".scroll_appear");
