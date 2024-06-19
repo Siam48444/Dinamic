f0784482f7e37420d8443a0fce95b61b18925e38
Siam <siam48444@gmail.com>
2024-06-19 18:46:55 +0600

    Main

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var tl = gsap.timeline();
 // Load animation.
 gsap.from(".load_appear", {
     opacity: 0,
-    y: 20,
+    y: 30,
     duration: 0.5,
     stagger: 0.1,
     delay: 0.3,
@@ -13,3 +13,4 @@ gsap.from(".load_appear", {
 // Vision button interactivity.
 const vision_btn = document.querySelectorAll(".vision_btn");
 
+vision_btn.forEach()
